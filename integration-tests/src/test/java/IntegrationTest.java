e7afb8903441d4b091b723df65a2eb793d483941
Nicolas <3923558+nhomble@users.noreply.github.com>
2025-01-20 15:13:08 -0500

    ci: adds post serve tests for new process instances and event publishing

--- integration-tests/src/test/java/IntegrationTest.java
+++ integration-tests/src/test/java/IntegrationTest.java
@@ -1,8 +1,16 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
+import io.camunda.operate.CamundaOperateClient;
+import io.camunda.operate.CamundaOperateClient.Builder;
+import io.camunda.operate.auth.SimpleAuthentication;
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.DeploymentEvent;
+import io.camunda.zeebe.client.api.response.ProcessInstanceResult;
 import java.nio.file.Path;
 import java.time.Duration;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,25 +23,59 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 public class IntegrationTest {
 
   static Logger log = LoggerFactory.getLogger(IntegrationTest.class);
+  static Path PROJECT_ROOT = Path.of("..");
+  static Path ZEEBE_RESOURCES = PROJECT_ROOT.resolve(Path.of("local", "zeebe-resources"));
+
+  static {
+    log.info("PROJECT_ROOT={}", PROJECT_ROOT.toAbsolutePath());
+  }
 
   // create via dc because that's how I am going to debug the compose anyway instead of learning yet
-  // another
-  // testcontainer builder syntax
+  // another testcontainer builder syntax
   @Container
   static ComposeContainer dc =
-      new ComposeContainer(Path.of("..", "local", "docker-compose.integration-test.yaml").toFile())
+      new ComposeContainer(
+              PROJECT_ROOT
+                  .resolve(Path.of("local", "docker-compose.integration-test.yaml"))
+                  .toFile())
+          .withCopyFilesInContainer(PROJECT_ROOT.toString())
           .withLogConsumer("zeebe-mock", new Slf4jLogConsumer(log).withPrefix("zeebe-mock"))
           .withLogConsumer("zeebe", new Slf4jLogConsumer(log).withPrefix("zeebe"))
-          .withPull(true)
-          .withLocalCompose(true);
+          .withPull(true);
 
   static ZeebeClient zeebeClient = ZeebeClient.newClientBuilder().usePlaintext().build();
+  static CamundaOperateClient operateClient;
 
-  @Test
-  void checkTopology() {
+  @BeforeAll
+  static void startup() {
+    awaitTopology();
+    awaitOperateClient();
+  }
+
+  static void awaitOperateClient() {
+    // defined in docker-compose.integration-test.yaml
+    String operateURL = "http://localhost:8081";
+    await("await operate client bootstrap")
+        .atMost(Duration.ofSeconds(30))
+        .until(
+            () -> {
+              try {
+                operateClient =
+                    new Builder()
+                        .operateUrl(operateURL)
+                        // default operate login
+                        .authentication(new SimpleAuthentication("demo", "demo", operateURL))
+                        .build();
+                return true;
+              } catch (Exception e) {
+                return false;
+              }
+            });
+  }
+
+  static void awaitTopology() {
     await()
-        .alias("zeebe topology (healthcheck)")
-        .atLeast(Duration.ofSeconds(15))
+        .alias("await zeebe topology (healthcheck) has brokers")
         .atMost(Duration.ofSeconds(60))
         .until(
             () -> {
@@ -43,5 +85,66 @@ public class IntegrationTest {
                 return false;
               }
             });
+  }
+
+  void awaitProcessDefinitionDeployment(DeploymentEvent event) {
+    // assume just 1 process deployed at a time, which is typical
+    long key = event.getProcesses().get(0).getProcessDefinitionKey();
+    await("await process definition key=" + key + " exists")
+        .atMost(Duration.ofSeconds(60))
+        .until(
+            () -> {
+              try {
+                return operateClient.getProcessDefinition(key).getKey().equals(key);
+              } catch (Exception e) {
+                return false;
+              }
+            });
+  }
+
+  void deployResource(Path path) {
+    DeploymentEvent deploy =
+        zeebeClient
+            .newDeployResourceCommand()
+            .addResourceFile(path.toFile().getAbsolutePath())
+            .send()
+            .join();
+    awaitProcessDefinitionDeployment(deploy);
+  }
+
+  @Test
+  void simpleWorkflow() {
+    deployResource(ZEEBE_RESOURCES.resolve("complete-simple-workflow.bpmn"));
+
+    ProcessInstanceResult result =
+        zeebeClient
+            .newCreateInstanceCommand()
+            .bpmnProcessId("SimpleComplete")
+            .latestVersion()
+            .withResult()
+            .send()
+            .join();
+    assertEquals("world", result.getVariable("hello"));
+  }
+
+  @Test
+  void parentTriggersChildWorkflow() {
+    deployResource(ZEEBE_RESOURCES.resolve("parent-awaits-child.bpmn"));
+    deployResource(ZEEBE_RESOURCES.resolve("child-events-parent.bpmn"));
+
+    String id = UUID.randomUUID().toString();
+    ProcessInstanceResult result =
+        zeebeClient
+            .newCreateInstanceCommand()
+            .bpmnProcessId("ParentAwaitsChild")
+            .latestVersion()
+            .variable("id", id)
+            .withResult()
+            .requestTimeout(Duration.ofSeconds(30))
+            .send()
+            .join();
+    assertEquals(id, result.getVariable("id"));
+    assertEquals("triggered child workflow", result.getVariable("parent"));
+    assertEquals("trigger event", result.getVariable("child"));
   }
 }
