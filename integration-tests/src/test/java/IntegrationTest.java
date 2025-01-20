f1a35142b59b5269335096e5f61471f010a7f4ca
nhomble <nhomble@terpmail.umd.edu>
2025-01-20 12:01:12 -0500

    chore: adds better logging

--- integration-tests/src/test/java/IntegrationTest.java
+++ integration-tests/src/test/java/IntegrationTest.java
@@ -3,12 +3,17 @@ import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 import io.camunda.zeebe.client.ZeebeClient;
 import java.nio.file.Path;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class IntegrationTest {
+
+  static Logger log = LoggerFactory.getLogger(IntegrationTest.class);
 
   // create via dc because that's how I am going to debug the compose anyway instead of learning yet
   // another
@@ -16,6 +21,8 @@ public class IntegrationTest {
   @Container
   static ComposeContainer dc =
       new ComposeContainer(Path.of("..", "local", "docker-compose.integration-test.yaml").toFile())
+          .withLogConsumer("zeebe-mock", new Slf4jLogConsumer(log))
+          .withLogConsumer("zeebe", new Slf4jLogConsumer(log))
           .withLocalCompose(true);
 
   static ZeebeClient zeebeClient = ZeebeClient.newClientBuilder().usePlaintext().build();
