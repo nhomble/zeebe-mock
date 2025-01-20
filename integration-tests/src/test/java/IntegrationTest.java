afcb34a2714339257a68ded433e46298f61b418e
Nicolas <3923558+nhomble@users.noreply.github.com>
2025-01-20 13:03:42 -0500

    ci: fix mvn test
    * ci: fix mvn test
    * chore: adds better logging
    * chore: adjust topology test timeout
    * chore: adds dependencies
    * chore: fix ci

--- integration-tests/src/test/java/IntegrationTest.java
+++ integration-tests/src/test/java/IntegrationTest.java
@@ -2,13 +2,19 @@ import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
 import io.camunda.zeebe.client.ZeebeClient;
 import java.nio.file.Path;
+import java.time.Duration;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.ComposeContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public class IntegrationTest {
+
+  static Logger log = LoggerFactory.getLogger(IntegrationTest.class);
 
   // create via dc because that's how I am going to debug the compose anyway instead of learning yet
   // another
@@ -16,6 +22,9 @@ public class IntegrationTest {
   @Container
   static ComposeContainer dc =
       new ComposeContainer(Path.of("..", "local", "docker-compose.integration-test.yaml").toFile())
+          .withLogConsumer("zeebe-mock", new Slf4jLogConsumer(log).withPrefix("zeebe-mock"))
+          .withLogConsumer("zeebe", new Slf4jLogConsumer(log).withPrefix("zeebe"))
+          .withPull(true)
           .withLocalCompose(true);
 
   static ZeebeClient zeebeClient = ZeebeClient.newClientBuilder().usePlaintext().build();
@@ -23,6 +32,9 @@ public class IntegrationTest {
   @Test
   void checkTopology() {
     await()
+        .alias("zeebe topology (healthcheck)")
+        .atLeast(Duration.ofSeconds(15))
+        .atMost(Duration.ofSeconds(60))
         .until(
             () -> {
               try {
