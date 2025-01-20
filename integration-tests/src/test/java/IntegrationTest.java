30fc508f3701604188357a4c04b53d39a98b03bc
nhomble <nhomble@terpmail.umd.edu>
2025-01-20 15:06:01 -0500

    ci: fix test

--- integration-tests/src/test/java/IntegrationTest.java
+++ integration-tests/src/test/java/IntegrationTest.java
@@ -1,12 +1,16 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
+import io.camunda.operate.CamundaOperateClient;
+import io.camunda.operate.CamundaOperateClient.Builder;
+import io.camunda.operate.auth.SimpleAuthentication;
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.api.response.DeploymentEvent;
 import io.camunda.zeebe.client.api.response.ProcessInstanceResult;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.UUID;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,15 +44,38 @@ public class IntegrationTest {
           .withPull(true);
 
   static ZeebeClient zeebeClient = ZeebeClient.newClientBuilder().usePlaintext().build();
+  static CamundaOperateClient operateClient;
 
-  @BeforeEach
-  void setup() {
+  @BeforeAll
+  static void startup() {
     awaitTopology();
+    awaitOperateClient();
   }
 
-  void awaitTopology() {
+  static void awaitOperateClient() {
+    // defined in docker-compose.integration-test.yaml
+    String operateURL = "http://localhost:8081";
+    await("await operate client bootstrap")
+        .atMost(Duration.ofSeconds(30))
+        .until(
+            () -> {
+              try {
+                operateClient =
+                    new Builder()
+                        .operateUrl(operateURL)
+                        // default operate login
+                        .authentication(new SimpleAuthentication("demo", "demo", operateURL))
+                        .build();
+                return true;
+              } catch (Exception e) {
+                return false;
+              }
+            });
+  }
+
+  static void awaitTopology() {
     await()
-        .alias("zeebe topology (healthcheck)")
+        .alias("await zeebe topology (healthcheck) has brokers")
         .atMost(Duration.ofSeconds(60))
         .until(
             () -> {
@@ -60,14 +87,35 @@ public class IntegrationTest {
             });
   }
 
+  void awaitProcessDefinitionDeployment(DeploymentEvent event) {
+    // assume just 1 process deployed at a time, which is typical
+    long key = event.getProcesses().get(0).getProcessDefinitionKey();
+    await("await process definition key=" + key + " exists")
+        .atMost(Duration.ofSeconds(60))
+        .until(
+            () -> {
+              try {
+                return operateClient.getProcessDefinition(key).getKey().equals(key);
+              } catch (Exception e) {
+                return false;
+              }
+            });
+  }
+
+  void deployResource(Path path) {
+    DeploymentEvent deploy =
+        zeebeClient
+            .newDeployResourceCommand()
+            .addResourceFile(path.toFile().getAbsolutePath())
+            .send()
+            .join();
+    awaitProcessDefinitionDeployment(deploy);
+  }
+
   @Test
   void simpleWorkflow() {
-    zeebeClient
-        .newDeployResourceCommand()
-        .addResourceFile(
-            ZEEBE_RESOURCES.resolve("complete-simple-workflow.bpmn").toFile().getAbsolutePath())
-        .send()
-        .join();
+    deployResource(ZEEBE_RESOURCES.resolve("complete-simple-workflow.bpmn"));
+
     ProcessInstanceResult result =
         zeebeClient
             .newCreateInstanceCommand()
@@ -81,18 +129,9 @@ public class IntegrationTest {
 
   @Test
   void parentTriggersChildWorkflow() {
-    zeebeClient
-        .newDeployResourceCommand()
-        .addResourceFile(
-            ZEEBE_RESOURCES.resolve("parent-awaits-child.bpmn").toFile().getAbsolutePath())
-        .send()
-        .join();
-    zeebeClient
-        .newDeployResourceCommand()
-        .addResourceFile(
-            ZEEBE_RESOURCES.resolve("child-events-parent.bpmn").toFile().getAbsolutePath())
-        .send()
-        .join();
+    deployResource(ZEEBE_RESOURCES.resolve("parent-awaits-child.bpmn"));
+    deployResource(ZEEBE_RESOURCES.resolve("child-events-parent.bpmn"));
+
     String id = UUID.randomUUID().toString();
     ProcessInstanceResult result =
         zeebeClient
@@ -101,6 +140,7 @@ public class IntegrationTest {
             .latestVersion()
             .variable("id", id)
             .withResult()
+            .requestTimeout(Duration.ofSeconds(30))
             .send()
             .join();
     assertEquals(id, result.getVariable("id"));
