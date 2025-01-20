38bb7c2388778ed5525d3542a9a20f26cdf50d40
nhomble <nhomble@terpmail.umd.edu>
2025-01-20 11:38:52 -0500

    chore: adds post serve tests for new process instances and event publishing

--- integration-tests/src/test/java/IntegrationTest.java
+++ integration-tests/src/test/java/IntegrationTest.java
@@ -1,16 +1,12 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testcontainers.shaded.org.awaitility.Awaitility.await;
 
-import io.camunda.operate.CamundaOperateClient;
-import io.camunda.operate.CamundaOperateClient.Builder;
-import io.camunda.operate.auth.SimpleAuthentication;
 import io.camunda.zeebe.client.ZeebeClient;
-import io.camunda.zeebe.client.api.response.DeploymentEvent;
 import io.camunda.zeebe.client.api.response.ProcessInstanceResult;
 import java.nio.file.Path;
 import java.time.Duration;
 import java.util.UUID;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,56 +22,28 @@ public class IntegrationTest {
   static Path PROJECT_ROOT = Path.of("..");
   static Path ZEEBE_RESOURCES = PROJECT_ROOT.resolve(Path.of("local", "zeebe-resources"));
 
-  static {
-    log.info("PROJECT_ROOT={}", PROJECT_ROOT.toAbsolutePath());
-  }
-
   // create via dc because that's how I am going to debug the compose anyway instead of learning yet
   // another testcontainer builder syntax
   @Container
   static ComposeContainer dc =
-      new ComposeContainer(
-              PROJECT_ROOT
-                  .resolve(Path.of("local", "docker-compose.integration-test.yaml"))
-                  .toFile())
-          .withCopyFilesInContainer(PROJECT_ROOT.toString())
+      new ComposeContainer(PROJECT_ROOT
+          .resolve(Path.of("local", "docker-compose.integration-test.yaml"))
+          .toFile())
           .withLogConsumer("zeebe-mock", new Slf4jLogConsumer(log).withPrefix("zeebe-mock"))
           .withLogConsumer("zeebe", new Slf4jLogConsumer(log).withPrefix("zeebe"))
           .withPull(true);
 
   static ZeebeClient zeebeClient = ZeebeClient.newClientBuilder().usePlaintext().build();
-  static CamundaOperateClient operateClient;
 
-  @BeforeAll
-  static void startup() {
+  @BeforeEach
+  void setup() {
     awaitTopology();
-    awaitOperateClient();
   }
 
-  static void awaitOperateClient() {
-    // defined in docker-compose.integration-test.yaml
-    String operateURL = "http://localhost:8081";
-    await("await operate client bootstrap")
-        .atMost(Duration.ofSeconds(30))
-        .until(
-            () -> {
-              try {
-                operateClient =
-                    new Builder()
-                        .operateUrl(operateURL)
-                        // default operate login
-                        .authentication(new SimpleAuthentication("demo", "demo", operateURL))
-                        .build();
-                return true;
-              } catch (Exception e) {
-                return false;
-              }
-            });
-  }
-
-  static void awaitTopology() {
+  void awaitTopology() {
     await()
-        .alias("await zeebe topology (healthcheck) has brokers")
+        .alias("zeebe topology (healthcheck)")
+        .atLeast(Duration.ofSeconds(15))
         .atMost(Duration.ofSeconds(60))
         .until(
             () -> {
@@ -87,35 +55,14 @@ public class IntegrationTest {
             });
   }
 
-  void awaitProcessDefinitionDeployment(DeploymentEvent event) {
-    // assume just 1 process deployed at a time, which is typical
-    long key = event.getProcesses().get(0).getProcessDefinitionKey();
-    await("await process definition key=" + key + " exists")
-        .atMost(Duration.ofSeconds(60))
-        .until(
-            () -> {
-              try {
-                return operateClient.getProcessDefinition(key).getKey().equals(key);
-              } catch (Exception e) {
-                return false;
-              }
-            });
-  }
-
-  void deployResource(Path path) {
-    DeploymentEvent deploy =
-        zeebeClient
-            .newDeployResourceCommand()
-            .addResourceFile(path.toFile().getAbsolutePath())
-            .send()
-            .join();
-    awaitProcessDefinitionDeployment(deploy);
-  }
-
   @Test
   void simpleWorkflow() {
-    deployResource(ZEEBE_RESOURCES.resolve("complete-simple-workflow.bpmn"));
-
+    zeebeClient
+        .newDeployResourceCommand()
+        .addResourceFile(
+            ZEEBE_RESOURCES.resolve("complete-simple-workflow.bpmn").toFile().getAbsolutePath())
+        .send()
+        .join();
     ProcessInstanceResult result =
         zeebeClient
             .newCreateInstanceCommand()
@@ -129,9 +76,18 @@ public class IntegrationTest {
 
   @Test
   void parentTriggersChildWorkflow() {
-    deployResource(ZEEBE_RESOURCES.resolve("parent-awaits-child.bpmn"));
-    deployResource(ZEEBE_RESOURCES.resolve("child-events-parent.bpmn"));
-
+    zeebeClient
+        .newDeployResourceCommand()
+        .addResourceFile(
+            ZEEBE_RESOURCES.resolve("parent-awaits-child.bpmn").toFile().getAbsolutePath())
+        .send()
+        .join();
+    zeebeClient
+        .newDeployResourceCommand()
+        .addResourceFile(
+            ZEEBE_RESOURCES.resolve("child-events-parent.bpmn").toFile().getAbsolutePath())
+        .send()
+        .join();
     String id = UUID.randomUUID().toString();
     ProcessInstanceResult result =
         zeebeClient
@@ -140,7 +96,6 @@ public class IntegrationTest {
             .latestVersion()
             .variable("id", id)
             .withResult()
-            .requestTimeout(Duration.ofSeconds(30))
             .send()
             .join();
     assertEquals(id, result.getVariable("id"));
